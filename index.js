ae45b207fa596d4b54a4bdd20578f1e83850dbf1
naomicherono <nonniecheronoh12@gmail.com>
2023-06-09 21:04:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+    let messages = [];
+
+for (let i = 0; i < names.length; i++) {
+  let message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+  messages.push(message);
+}
+
+return messages;
+  }
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
